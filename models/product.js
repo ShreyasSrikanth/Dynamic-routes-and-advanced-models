b608f7d38112305dbc11720b5be24cf65b665f08
Shreyas Srikanth <sshreyas567@gmail.com>
2023-11-21 02:33:55 +0530

    edit and delete working perfectly

--- models/product.js
+++ models/product.js
@@ -17,21 +17,33 @@ const getProductsFromFile = cb => {
   });
 };
 
+//same product for creating a new product and updating
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
-    this.title = title;
+  constructor(id, title, imageUrl, description, price) {         
+    this.id = id                                         // this id we are taking null if the product is new else we are receiving id
+    this.title = title;                                   
     this.imageUrl = imageUrl;
     this.description = description;
     this.price = price;
   }
 
   save() {
-    this.id = Math.random().toString();
+    
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
+      if(this.id){
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      } else {
+          this.id = Math.random().toString();
+          products.push(this);
+          fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
       });
+      }
     });
   }
 
@@ -43,6 +55,15 @@ module.exports = class Product {
     getProductsFromFile(products => {
       const product = products.find(p=>p.id===id);
       cb(product);
+    })
+  }
+
+  static removeProduct(id){
+    getProductsFromFile(product =>{
+      const deleteProduct = product.filter(p=>p.id!==id);
+      fs.writeFile(p, JSON.stringify(deleteProduct), err => {
+        console.log(err);
+    });
     })
   }
 };
