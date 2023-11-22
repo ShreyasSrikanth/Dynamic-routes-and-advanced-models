438f9d67aaea6172e3df7c4a0b070ff6f39c5cd0
Shreyas Srikanth <sshreyas567@gmail.com>
2023-11-22 22:22:12 +0530

    Succesfully connected to database

---
+++ util/database.js
@@ -0,0 +1,17 @@
+//This file gives acces to pool and is being used by exporting 
+
+const mysql = require('mysql2');
+
+//create connection pool - (new connection from that pool which runs multiple connections for multiple queries)
+//This pool is finished when application shuts down
+
+const pool = mysql.createPool({
+    host : ' localhost',             //host is running locally on localhost so localhost
+    user: 'root',                    // user is we have choosen root on DB
+    database: 'node-complete',       //Schema name
+    password: 'root'                 //password
+});
+
+module.exports = pool.promise();        //exporting
+
+//instead of call backs we are using promise because it allows us to run it asynchronously and write our code in a more asynchronous way
