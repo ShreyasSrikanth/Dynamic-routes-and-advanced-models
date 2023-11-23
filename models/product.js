7e8d636e8e6ae9c057ffd3924c60d3c5e8c6a0ae
Shreyas Srikanth <sshreyas567@gmail.com>
2023-11-23 19:09:15 +0530

    Modified the sql server in the previous task

--- models/product.js
+++ models/product.js
@@ -1,34 +1,28 @@
-const db = require('../util/database');
-const cart = require('./cart');
+const Sequelize = require('sequelize');
 
+const sequelize = require('../util/database');  // with these two imported we will now define models that is handled bny sequelize
 
-//same product for creating a new product and updating
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {         
-    this.id = id                                         // this id we are taking null if the product is new else we are receiving id
-    this.title = title;                                   
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+//define new model by model name first arg is model name , second arg defines structure of models
+const Product = sequelize.define('product',{
+  id:{
+    type: Sequelize.INTEGER,
+    autoIncrement : true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title:Sequelize.STRING,                   //type this is shortcut method of setting type if its the only one required to set
+  price:{
+    type:Sequelize.DOUBLE,
+    allowNull:false
+  },
+  imageUrl:{
+    type: Sequelize.STRING,
+    allowNull:false
+  },
+  description: {
+    type:Sequelize.STRING,
+    allowNull:false
   }
+});
 
-  //USE ? TO AVOID SQL INJECTION
-  save() {
-    return db.execute(
-      'INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.description, this.imageUrl]
-    );
-  }
-
-  static removeProduct(id){
-    db.execute('DELETE FROM products WHERE products.id = ?',[id]);
-  }
-  
-  static fetchAll() {
-    return db.execute('SELECT * FROM products'); 
-  }
-
-  static findById(id){
-    return db.execute('SELECT * FROM products WHERE products.id =?',[id]);
-  }
-};
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,17 +1,9 @@
-//This file gives acces to pool and is being used by exporting 
+const Sequelize = require('sequelize');           // import the sequelize lib
 
-const mysql = require('mysql2');
-
-//create connection pool - (new connection from that pool which runs multiple connections for multiple queries)
-//This pool is finished when application shuts down
-
-const pool = mysql.createPool({
-    host : 'localhost',             //host is running locally on localhost so localhost
-    user: 'root',                    // user is we have choosen root on DB
-    database: 'node-complete',       //Schema name
-    password: 'root'                 //password
+//in sequelize class pass an argument that is the schema name in sql workbench and root username and password dialect is the type of sql we are using for us in this case it is mysql
+const sequelize = new Sequelize('node-complete','root','root' , {
+    dialect: 'mysql' , 
+    host:'localhost'
 });
 
-module.exports = pool.promise();        //exporting
-
-//instead of call backs we are using promise because it allows us to run it asynchronously and write our code in a more asynchronous way
+module.exports = sequelize;
